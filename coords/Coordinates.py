7362e9e065231e046432de67a5c479213d8e54e7
yul.korolenko <yulkorolenko@yandex-team.ru>
2024-02-19 21:07:52 +0300

    fix coordinates

--- coords/Coordinates.py
+++ coords/Coordinates.py
@@ -22,11 +22,18 @@ class HandMadeCoordinates(CoordinateBase):
         )
         self.file_path = file_path
         self.cur_pos = -1
-
-        with open(file_path, "r") as file:
-            self.numbers_list = [(int(t.split()[0]), int(t.split()[1])) for t in file.readlines()]
             
-        # TO DO: check format (правильные границы хромосом)
+        try:
+            with open(file_path, "r") as file:
+                self.numbers_list = [(int(t.split()[0]), int(t.split()[1]), int(t.split()[2]))
+                                  for t in file.readlines()]
+        except FileNotFoundError:
+            raise Exception("The file could not be opened: %s" % file_path)
+            
+        # check boarders of chromosome
+        for elem_in_list in self.numbers_list:
+            if (chr_bounds[elem_in_list[0]].lenght <= elem_in_list[2]):
+                raise RuntimeError("Произошла ошибка!")
 
     def get_next_coord(self):
         self.cur_pos += 1
@@ -46,15 +53,30 @@ class RandomCoordinates(CoordinateBase):
         )
         random.seed(seed)
         self.state = random.getstate()
+        self.full_length = 0
+        for cur_chr in chr_bounds:
+            self.full_length += cur_chr.lenght
+
+    def get_correct_chr(self, rnd_start : int):
+        for cur_bounds in self.chr_bounds:
+            if cur_bounds.start_pos <= rnd_start < cur_bounds.start_pos + cur_bounds.lenght:
+                return cur_bounds
 
     def get_next_coord(self):
         random.setstate(self.state)
-        chr_rnd_number = random.randint(0, len(self.chromosomes) - 1)
-        # TO DO: если одна хромосома больше другой, должны ли мы в нее в два раза чаще попадать?
-        chr_rnd = self.chromosomes[chr_rnd_number]
-        start_rnd = random.randint(0, self.chr_bounds[chr_rnd].lenght - self.window_size)
-        self.state = random.getstate()
-        return (chr_rnd, start_rnd)
+
+        find_chr = False
+        while(not find_chr):
+            rnd_start = random.randint(0, self.full_length)
+            self.state = random.getstate()
+            rnd_bounds = self.get_correct_chr(rnd_start)
+            if (rnd_start + self.window_size < rnd_bounds.start_pos + rnd_bounds.lenght):
+                return (rnd_bounds.number,
+                        rnd_start - rnd_bounds.start_pos,
+                        rnd_start - rnd_bounds.start_pos + self.window_size)
+        
+
+
 
 
 
--- coords/test.py
+++ coords/test.py
@@ -7,18 +7,20 @@ class TestReadLinesWithTwoNumbers(unittest.TestCase):
 
     chrom_info = [Chromosome_Info(1, 100, 200),
                         Chromosome_Info(2, 300, 400),
-                        Chromosome_Info(3, 700, 300),
+                        Chromosome_Info(3, 700, 800),
                         Chromosome_Info(4, 1100, 100)]  
 
     def test_hand_made_coord(self):
-        coords = HandMadeCoordinates(self.chrom_info, 100, [i for i in range(4)], "coords/handMade.txt")
-        self.assertEqual(coords.get_next_coord(), (1, 23))
-        self.assertEqual(coords.get_next_coord(), (0, 100))
-        self.assertEqual(coords.get_next_coord(), (3, 10))
-        self.assertEqual(coords.get_next_coord(), (5, 2))
-        self.assertEqual(coords.get_next_coord(), (1, 23))
+        coords = HandMadeCoordinates(self.chrom_info, 100, [i for i in range(4)], "coords/test/handMade.txt")
+        self.assertEqual(coords.get_next_coord(), (1, 23, 43))
+        self.assertEqual(coords.get_next_coord(), (0, 100, 120))
+        self.assertEqual(coords.get_next_coord(), (2, 10, 30))
+        self.assertEqual(coords.get_next_coord(), (1, 2, 23))
+        self.assertEqual(coords.get_next_coord(), (1, 23, 43))
 
-    # TODO: написать тест на проверку неправильных координат у HandMadeCoord
+    def test_wrong_hand_made(self):
+        with self.assertRaises(Exception):
+            coords = HandMadeCoordinates(self.chrom_info, 100, [i for i in range(4)], "coords/test/wrongHandMade.txt")
         
     def test_random_coord(self):  
         coords = RandomCoordinates(self.chrom_info, 100, [i for i in range(4)], 3)
