77377d0a44409de0561266a7d81b3575c6563acd
YuliaKorolenko <ojpochemy@dna-loader.ru-central1.internal>
2024-05-01 19:55:37 +0000

    change rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-printf "%s\n" $SCRIPT_DIR
+printf "Current directory: %s\n" $SCRIPT_DIR
 DNALOADER_DIR="$SCRIPT_DIR/dnaloader"
-printf "%s\n" $DNALOADER_DIR
-export PYTHONPATH=$DNALOADER_DIR:$PYTHONPATH
+if [[ ":$PYTHONPATH:" != *":$DNALOADER_DIR:"* ]]; then
+    export PYTHONPATH=$DNALOADER_DIR:$PYTHONPATH
+fi
 echo $PYTHONPATH
+printf "Creating enviroment\n"
 conda env create -f "environment.yml"
 conda activate bwenv
 python "$SCRIPT_DIR/setup.py" build_ext --inplace
