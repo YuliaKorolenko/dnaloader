5a362578347c9945dd477ed33cea5bebdc649f6e
YuliaKorolenko <ojpochemy@dna-loader.ru-central1.internal>
2024-05-01 19:07:06 +0000

    move to dnaloader

--- characteristics/__init__.py
+++
@@ -1,6 +0,0 @@
-from .Characteristics import Characteristic, CharacteristicBigWig, CharacteristicBigWigCSR
-from .CharacteristicsHiC import CharacteristicHiCColer, CharacteristicHiCWithLimit
-from .CharacteristicFullHiC import CharacteristicFullHiC, HiCCollerFormat
-
-__all__ = ['Characteristic', 'CharacteristicBigWig', 'CharacteristicBigWigCSR',
-           'CharacteristicHiCColer', 'CharacteristicHiCWithLimit', 'CharacteristicFullHiC', 'HiCCollerFormat']
